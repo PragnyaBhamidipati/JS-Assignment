a98071ec6c03fadf55391f9600d195701245e488
PragnyaBhamidipati <68955720+PragnyaBhamidipati@users.noreply.github.com>
2020-07-30 12:46:46 +0530

    Update Assignment2.js

--- Assignment2.js
+++ Assignment2.js
@@ -1,48 +1,48 @@
 class Fun {
     constructor(arrayData) {
         this.data = arrayData;
+        this.i=0;
     }
     next() {
-        i=i+1;
-        return arrayData[i];
+        this.i=this.i+1;
+        return this.data[this.i];
     }
     current() {
-        return arrayData[i];
+        return this.data[this.i];
     }
     previous() {
-        i=i-1;
-        return arrayData[i];
+        this.i=this.i-1;
+        return this.data[this.i];
     }
     reset() {
-        i=0;
-        return arrayData[i];
+        this.i=0;
+        return this.data[this.i];
     }
     top() {
-        return Math.max(...arrayData);
+        return this.data[this.data.length-1];
     }
     floor() {
-        return Math.min(...arrayData);
+        return this.data[0];
     }
     currentIndex() {
-        return i;
+        return this.i;
     }
     previousIndex() {
-        return i-1;
+        return this.i-1;
     }
     nextIndex() {
-        return i+1;
+        return this.i+1;
     }
 }
 let arrayData = new Array(1,2,3,4,5,6);
-let i=0;
 let users = new Fun(arrayData);
-let ne = users.next();
-let pr = users.previous();
-let cr = users.current();
-let t = users.top();
-let ci = users.currentIndex();
-let f = users.floor();
-let pi = users.previousIndex();
-let ni = users.nextIndex();
-let r = users.reset();
-let n = users.next();
+console.log(users.next());
+console.log(users.previous());
+console.log(users.current());
+console.log(users.top());
+console.log(users.currentIndex());
+console.log(users.floor());
+console.log(users.previousIndex());
+console.log(users.nextIndex());
+console.log(users.reset());
+console.log(users.next());
